3a9d87852f039ac9d1645b5a7719904babd93ab4
micro-hawk <dasvikas0071@gmail.com>
2020-12-02 20:56:10 +0530

    Reversing Array

--- Arrays/reverse_array.cpp
+++ Arrays/reverse_array.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int t; cin >> t;
+    while (t--){
     vector<int> v;
     int n; cin >> n;
     v.reserve(n);
@@ -13,4 +15,6 @@ int main(){
     for(const auto e:v ){
         cout << e << " ";
     }
+}
+cout << "\n";
 }
