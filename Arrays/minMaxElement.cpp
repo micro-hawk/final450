f431e047df8362e3b9d8f77c73ad3052e2aac08f
micro-hawk <dasvikas0071@gmail.com>
2020-12-02 21:21:56 +0530

    Minimum And Maximum Element in array

---
+++ Arrays/minMaxElement.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >>n;
+        vector<int> v; 
+        v.reserve(n);
+        for (int i = 0; i < n; i++)
+        {
+            int input; cin >> input;
+            v.push_back(input);
+        }
+        
+        sort(v.begin(), v.end());
+        cout << "Min element is: " << v[0]<< " Max element is: " << v[n-1];
+        cout << endl;
+    }
+    
+}
