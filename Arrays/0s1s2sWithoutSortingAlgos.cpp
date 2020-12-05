42a222ea13bf96b19fb8bb7c29d40db801914dd6
micro-hawk <dasvikas0071@gmail.com>
2020-12-06 02:07:04 +0530

    Umm Using sorting algo

---
+++ Arrays/0s1s2sWithoutSortingAlgos.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w", stdout);
+	#endif
+
+	std::vector<int> v; int n; cin >> n;
+	v.reserve(n);
+	for (int i = 0; i < n; ++i)
+		{
+			int input; cin >> input;
+			v.push_back(input);
+		}	sort(v.begin(), v.end());
+	for(const auto e:v){
+		cout << e << " ";
+	}
+	cout << "\n";
+}
