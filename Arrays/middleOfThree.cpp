b4921f19ae30dc1ceef9181019efda6fcdc65536
micro-hawk <dasvikas0071@gmail.com>
2020-12-05 20:33:27 +0530

    learning sublime Commit
    Signed-off-by: micro-hawk <dasvikas0071@gmail.com>

--- Arrays/middleOfThree.cpp
+++ Arrays/middleOfThree.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-	
+	#ifndef ONLINE_JUDGE
+    freopen ("input.txt", "r", stdin);
+    freopen ("output.txt", "w", stdout);
+	#endif
+
+    int x,y,z; cin >> x >> y >> z;
+    // cout << x << " " << y<< " " << z << endl;
+    
+	return 0;
 }
