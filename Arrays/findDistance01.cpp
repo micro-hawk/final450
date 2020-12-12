07ef5da0221dca15e42d0cb17bd40179ab16d8cd
micro-hawk <dasvikas0071@gmail.com>
2020-12-12 21:37:12 +0530

    Distance Formulae

---
+++ Arrays/findDistance01.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main() {
+	// INPUT AND OUTPUT files
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w", stdout);
+	#endif
+
+	int t; cin >> t;
+	while(t--)
+	{
+	long long int x1,x2,y1,y2; cin >> x1>>y1>>x2>>y2;
+	float distance = sqrt(((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)));
+	cout << setprecision(13);
+	cout << distance;
+	return 0;
+	}
+}
