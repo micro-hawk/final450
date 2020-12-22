47bc79193806204ffaf1e16c881de2fb85bddcf0
micro-hawk <dasvikas0071@gmail.com>
2020-12-22 21:18:43 +0530

    remaining

---
+++ Arrays/kadanesAlgo.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main() {
+	// INPUT AND OUTPUT files
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w", stdout);
+	#endif
+
+	int n; cin >> n; int arr[n];
+	for(int i=0; i<n; i++)
+	{
+		cin >> arr[i] ;
+	} cout << "\n";
+
+	int meh, msf=INT_MIN;
+	return 0;
+	}
