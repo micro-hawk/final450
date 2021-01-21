e94753b83fe11dea2515b04ab0f2a1cfdcd6018e
micro-hawk <dasvikas0071@gmail.com>
2021-01-21 19:01:49 +0530

    inserting element

---
+++ Arrays/insertingArr.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void insert(){
+
+}
+void display(int *arr)
+{
+    for (int i=0; i<n; i++) 
+        cout << arr[i] << " ";
+}
+int main()
+{
+    int n; cin >> n;
+    int arr[n],i; 
+    for(i=0;i<n;i++)
+        cin >> arr[i];
+    
+}
